743aec53eb2dc4b7d3f9e7472553dca4ec81d057
kireetikoka <92074929+kireetikoka@users.noreply.github.com>
2021-10-23 10:53:58 +0530

    Add files via upload

---
+++ 2 points adding.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	//variable declarition
+	float cgpa1, cgpa2, sum;
+	
+	//intialization
+	cgpa1 = 8.5;
+	cgpa2 = 9.5;
+	
+	//calculation
+	sum = cgpa1+cgpa2;
+	
+	//output
+	printf("sum of %f and sum of %f is %f", cgpa1,cgpa2,sum);
+}
---
+++ variables.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+	// variable declaration
+	int age;
+	float cgpa;
+	char initial;
+	
+	// initialization
+	age = 20;
+	cgpa = 9.5;
+	initial = 'K';
+	// output
+	printf("age of the student is %d\n", age);
+	printf("cgpa of the student is %f\n",cgpa);
+	printf("intial of the student is %c", initial);
+}
